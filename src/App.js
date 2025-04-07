adaed8a5395cc883999a24814b596f7d3ad3b002
ABTech-07 <abandekar@clarku.edu>
2025-04-06 22:08:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import img1 from './1.jpg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <img src={img1}/>
+      <p> ForntEnd Caching</p> 
+    </div>
+  );
+}
+
+export default App;
